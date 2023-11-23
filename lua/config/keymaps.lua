1222c039f63edf65e24cd39eed172fcddef35480
victorinoLavida <victorinolavida@ciencias.unam.mx>
2023-11-22 22:11:07 -0600

    feat: new nvim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,63 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local disipline = require("victorinoLavida.discipline")
+disipline.cowboy()
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backward
+keymap.set("n", "dw", "vb_d")
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- jumplist
+keymap.set("n", "<C-m>", "<C-i>", opts)
+
+-- new tab
+keymap.set("n", "te", ":tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- split window
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+--move windows
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize windows
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
+
+-- Diagnostics
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
+
+-- other keymaps
+-- [[ Basic Keymaps ]]
+vim.keymap.set("n", ";pv", vim.cmd.Ex)
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  "craftzdog/solarized-osaka.nvim",
+  "chriskempson/base16-default-schemes",
+  lazy = true,
+  priority = 1000,
+  opts = function()
+    return {
+      transparent = true,
+    }
+  end,
+}
